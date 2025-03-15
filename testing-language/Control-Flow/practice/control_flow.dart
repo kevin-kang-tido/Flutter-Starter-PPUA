1436d60d99053129e0bca1818886c5eeac3210ad
kevin-kang-tido <kaykang84@gmail.com>
2025-03-15 20:16:33 +0700

    save control flow  🔥🐱‍👤

--- testing-language/Control-Flow/practice/control_flow.dart
+++ testing-language/Control-Flow/practice/control_flow.dart
@@ -18,4 +18,62 @@ void main() {
   }
 
   // TODO: 3: `if-else if` Ladder
+  int score = 99;
+  if (score >= 90) {
+    print("Grade: A");
+  } else if (score >= 80) {
+    print("Grade: B");
+  } else if (score >= 70) {
+    print("Grade: C");
+  } else {
+    print("you are filed");
+  }
+
+  // TODO: 4: Nested `if` Statements
+  int num1 = 25;
+
+  if (num1 > 0) {
+    if (num1 % 5 == 0) {
+      print("$num1 is a positive multiple of 5.");
+    } else {
+      print("$num1 is positive but not a multiple of 5.");
+    }
+  }
+  // TODO: 5:  Combining Conditions in `if` Statements
+  int age = 20;
+  String nationality = "USA";
+
+  if (age >= 18 && nationality == "USA") {
+    print("You are eligible to vote in the USA.");
+  } else {
+    print("You are not eligible to vote in the USA.");
+  }
+
+  // TODO:
+
+  print("======== Iteration Statements");
+  // TODO: 1: `for` Loop
+  for (int i = 1; i <= 5; i++) {
+    print("Iteration: $i");
+  }
+  // TODO: 2: `while` Loop
+  int count = 1;
+  while (count <= 5) {
+    print("Count: $count");
+    count++;
+  }
+
+  // TODO: 3: `do-while` Loop
+  int j = 1;
+  int count1 = 1;
+  do {
+    print("Count1: $count1");
+    count1++;
+  } while (count1 <= 5);
+  // TODO: 4: Iterating Over a Collection Using a `for` Loop
+  List<String> fruits = ["Apple", "Banana", "Cherry"];
+
+  for (int i = 0; i < fruits.length; i++) {
+    print("Fruit: ${fruits[i]}");
+  }
 }
